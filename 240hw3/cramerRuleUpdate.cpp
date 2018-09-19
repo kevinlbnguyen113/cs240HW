3599f50f2620182cf922d4fc26eef69aeaf82ca4
kevinlbnguyen113 <36432653+kevinlbnguyen113@users.noreply.github.com>
2018-09-18 18:01:51 -0700

    Update cramerRuleUpdate.cpp
    format and updated

--- 240hw3/cramerRuleUpdate.cpp
+++ 240hw3/cramerRuleUpdate.cpp
@@ -8,64 +8,58 @@ short top = 0;
 short bottom = 0;
 short zero = 0;
 short findXASM() {
-	_asm {
-			mov		ax, c
-			imul	e
-			mov		temp1, ax
-			mov		ax, b
-			imul	f
-			sub		temp1, ax
-			mov		ax, temp1
-			mov		top, ax
-
-			mov ax, a
-			imul ax, e
-			mov temp1, ax
-			mov ax, b
-			imul ax, d
-			sub temp1, ax
-			mov ax, temp1
-			mov bottom, ax
-
-		//	mov ax, 0
-			//mov bx, 0
-
-			//ALWAYS SET DX, TO TO ZERO 
-			//xor dx, dx
-			mov ax, top
+	_asm 
+	{
+			mov		ax, c;
+			imul		e;
+			mov		temp1, ax;
+			mov		ax, b;
+			imul		f;
+			sub		temp1, ax;
+			mov		ax, temp1;
+			mov		top, ax;
+			mov 		ax, a;
+			imul		ax, e;
+			mov		temp1, ax;
+			mov 		ax, b;
+			imul 		ax, d;
+			sub 		temp1, ax;
+			mov 		ax, temp1;
+			mov 		bottom, ax;
+			mov 		ax, top;
 			cwd
-			idiv bottom
-			mov x, ax
+			idiv 		bottom;
+			mov 		x, ax;
 	}
 	return x;
 }
 short findYASM() {
 	_asm
 	{
-		mov ax, a
-		imul ax, f
-		mov temp1, ax
-		mov ax, c
-		imul ax, d
+		mov 	ax, a
+		imul 	ax, f
+		mov 	temp1, ax
+		mov 	ax, c
+		imul 	ax, d
 
-		sub temp1, ax
-		mov ax, temp1
-		mov top, ax
+		sub 	temp1, ax
+		mov 	ax, temp1
+		mov 	top, ax
 
-		mov ax, a
-		imul ax, e
-		mov temp1, ax
-		mov ax, b
-		imul ax, d
+		mov 	ax, a
+		imul 	ax, e
+		mov 	temp1, ax
+		mov 	ax, b
+		imul 	ax, d
 
-		sub temp1, ax
-		mov ax, temp1
-		mov bottom, ax
+		sub 	temp1, ax
+		mov 	ax, temp1
+		mov 	bottom, ax
 
-		xor dx, dx //ALWAYS SET DX, TO TO ZERO 
-		mov ax, top
-		idiv bottom
-		mov y, ax
+		xor 	dx, dx //ALWAYS SET DX, TO TO ZERO 
+		mov 	ax, top
+		idiv 	bottom
+		mov 	y, ax
 	}
 	return y;
 }
@@ -75,17 +69,14 @@ int main()
 	cout << "This program sloves the system of" << endl;
 	cout << "aX+by=c" << endl;
 	cout << "dX + eY = f" << endl;
-	cout << "enter the values of a,b, and c" << endl;
+	cout << "enter the values of a,b, and c";
 	cin >> a >> b >> c;
-	cout << "enter the values of d, e , f " << endl;
+	cout << "enter the values of d, e , f ";
 	cin >> d >> e >> f;
 	findXASM();
 	findYASM();
-	//cout << top << endl;
-	//cout << bottom << endl; 
-
-	cout << "y " << y << endl;
-	cout << "x " << x << endl;
+	cout << "X = " << x << endl;
+	cout << "Y = " << y << endl;
 
 	system("PAUSE");
 	return 0;
