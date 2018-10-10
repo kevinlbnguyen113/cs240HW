72ee4c323377196532cd005c715194ffbd0c6c10
Spagecko <bennettlawrenz@gmail.com>
2018-10-10 00:08:55 -0700

    Update Asm_bubblesort.cpp
    Updated with better documentation of explaining the code and the important uses of ECX

--- cs240hw6/Asm_bubblesort.cpp
+++ cs240hw6/Asm_bubblesort.cpp
@@ -1,55 +1,48 @@
-﻿// ASStest1.cpp : Defines the entry point for the console application.
-//
-
 #include "stdafx.h"
 #include<iostream>
-using namespace std; 
-int a[5] = { 9,3,22,8,1 };  //0//4//8//12//16//20
-int countA = 5;
+using namespace std;
+int sizef = 5;
+int a[5] = { 7,2,4,5,1 };
 void print()
 {
-	for (int i = 0; i < countA; i++)
+	for (int i = 0; i < 5; i++)
 	{
 		cout << a[i] << " ";
 	}
 }
 
+
 int main()
 {
-	cout << "orginal array ";
+	cout << "original array "; 
 	print();
-	cout << endl;
+	cout << endl; 
 	_asm
 	{
-		mov ecx, countA
-		dec ecx
+		mov ecx, sizef // ECS MUST BE USED
+		dec ecx // <--- ecx must be used for this methhod
+		
+		nextScan: // moves to the next index 
+		mov ebx, ecx
+		mov eax,0 
 
-		nextscan : 
-			mov ebx, ecx
-			mov eax, 0
-
-				nextcomp :
-
-				mov edx, a[eax]
-				mov edi, a[eax+4] // asm																		
-				cmp edx, edi
-
-				jl noswap
-
-				mov a[eax], edi // swap
-				mov a[eax+4], edx// <--
-				noswap :
-				add eax, 4
-				sub ebx,1
-				jnz nextcomp
-
-				loop nextscan
+			Nextcomp:
+			mov edx, a[eax] // edx and edi will be used to store our indexs
+			mov edi, a[eax+4]
+			cmp edx,edi
+			jl noswap // jmp if less than 
+			mov a[eax], edi // <--- swap
+			mov a[eax+4], edx
+			noswap:
+			add eax,4
+			sub ebx,1
+			jnz nextcomp
+			loop nextScan // subtracts 1 from ecs regeistor and if ecs is zero it does not jump 
+		
 	}
 	cout << "new array ";
 	print();
 	cout << endl;
-
 	system("pause");
     return 0;
 }
-
